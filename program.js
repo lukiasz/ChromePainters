589ea344c33b85d89a51e1ceab69984de31d1fda
klapek <jaszczakw@gmail.com>
2013-05-07 00:02:01 +0200

    paintersMenager, sterowanie wieloma pedzlami

--- program.js
+++ program.js
@@ -6,9 +6,80 @@ var sphere;
 var grid;
 var grid3d;
 var painter;
+var painters =[];
+
+
+var paintersManager = {
+    gridSize: 512,
+    amount: 1,
+    init: function () {
+        var colors = ['red', 'blue', 'green', 'yellow'];
+        for (var i = 0; i < this.amount; i++) {
+            var painterTemp = new Painter({
+                startx: (this.gridSize - 200*(i-this.amount+1)) / 2,
+                starty: (this.gridSize - 200*(i-this.amount+1)) / 2,
+                angle: 0,
+                color: colors[i],
+                scene: scene,
+                gridSizeX: this.gridSize,
+                gridSizeY: this.gridSize
+            });
+            painterTemp.init();
+            painters[i] = painterTemp;
+        }
+    },
+    steering: function () {
+        var steeringKeys = [['W', 'A', 'S', 'D'], ["I", "J", "K", "L"], ["up", "left", "down", "right"]];
+        var delta = clock.getDelta();
+        var moveDistance = 200 * delta; // 200 pixels per second
+        var rotateAngle = Math.PI / 1.5 * delta; // pi/2 radians (90 degrees) per second
+        var j = 0;
+        for (var i = 0; i < this.amount; i++) {
+
+            if (keyboard.pressed(steeringKeys[i][0])) { //up
+                painters[i].goForward(1, grid.context);
+            }
+            if (keyboard.pressed(steeringKeys[i][2])) { //down
+                painters[i].goForward(-1, grid.context);
+            }
+
+            // rotate left/right/up/down
+            var rotation_matrix = new THREE.Matrix4().identity();
+            if (keyboard.pressed(steeringKeys[i][1])) { //left
+                painters[i].turnLeft(0.1);
+                rotation_matrix = new THREE.Matrix4().makeRotationY(rotateAngle);
+            }
+            if (keyboard.pressed(steeringKeys[i][3])) { //right
+                painters[i].turnLeft(-0.1);
+                rotation_matrix = new THREE.Matrix4().makeRotationY(-rotateAngle);
+            }
+        }
+
+
+        //-----------------Debugging-----------------------------
+        if (keyboard.pressed("R")) {
+            // do testow statystyk. mozna spokojnie wywalic
+            var statistics = new Statistics(grid.canvas);
+            statistics.getColorStats();
+            debugger;
+        }
+        if (keyboard.pressed("F"))
+            rotation_matrix = new THREE.Matrix4().makeRotationX(-rotateAngle);
+        if (keyboard.pressed("A") || keyboard.pressed("D") ||
+                keyboard.pressed("R") || keyboard.pressed("F")) {
+            //sphere.matrix.multiply(rotation_matrix);
+            //sphere.rotation.setEulerFromRotationMatrix(sphere.matrix);
+        }
+    }
+};
+
+
 
 init();
 animate();
+
+
+
 
 function initializeGrid() {
     // Grzesiu: Wspolrzedne na plansza 3D maja byc
@@ -28,16 +99,16 @@ function initializeGrid() {
     var gridSize = 512;
     grid = new CanvasMan(gridSize, gridSize);
     grid.init();
-    painter = new Painter({
-        startx: gridSize / 2,
-        starty: gridSize / 2,
-        angle: 0,
-        color: 'green',
-        scene: scene,
-        gridSizeX: gridSize,
-        gridSizeY: gridSize
-    });
-    painter.init();
+//    painter = new Painter({
+//        startx: gridSize / 2,
+//        starty: gridSize / 2,
+//        angle: 0,
+//        color: 'green',
+//        scene: scene,
+//        gridSizeX: gridSize,
+//        gridSizeY: gridSize
+//    });
+//    painter.init();
 
     grid3d = new Grid({
         scene: scene,
@@ -125,6 +196,8 @@ function init() {
 
     scene.fog = new THREE.FogExp2(0x999900, 0.00001);
     initializeGrid();
+    paintersManager.amount = 3;
+    paintersManager.init();
 }
 
 function animate() {
@@ -150,47 +223,17 @@ function update() {
     // Wykorzystaj clock.getDelta() do wszystkich ruchow. Miejsce
     // w ktorym pojawiaja sie pedzle moze byc takie jak w Battle Painters
     // albo dowolne inne tylko zeby na siebie nie nachodzily. 
-
-    var delta = clock.getDelta();
-    var moveDistance = 200 * delta; // 200 pixels per second
-    var rotateAngle = Math.PI / 1.5 * delta; // pi/2 radians (90 degrees) per second
-    // move forwards/backwards/left/right
-    if (keyboard.pressed("W")) {
-        painter.goForward(1, grid.context);
-
-    }
-    if (keyboard.pressed("S")) {
-        painter.goForward(-1, grid.context);
-
-    }
-
-    // rotate left/right/up/down
-    var rotation_matrix = new THREE.Matrix4().identity();
-    if (keyboard.pressed("A")) {
-        painter.turnLeft(0.1);
-        rotation_matrix = new THREE.Matrix4().makeRotationY(rotateAngle);
-    }
-    if (keyboard.pressed("D")) {
-        painter.turnLeft(-0.1);
-        rotation_matrix = new THREE.Matrix4().makeRotationY(-rotateAngle);
-    }
-    if (keyboard.pressed("R")) {
-			// do testow statystyk. mozna spokojnie wywalic
-			var statistics = new Statistics(grid.canvas);
-			statistics.getColorStats();
-			debugger;
-	}
-    if (keyboard.pressed("F"))
-        rotation_matrix = new THREE.Matrix4().makeRotationX(-rotateAngle);
-    if (keyboard.pressed("A") || keyboard.pressed("D") ||
-        keyboard.pressed("R") || keyboard.pressed("F")) {
-        //sphere.matrix.multiply(rotation_matrix);
-        //sphere.rotation.setEulerFromRotationMatrix(sphere.matrix);
-    }
-
+    
+    paintersManager.steering();
+    
+	
     controls.update();
     stats.update();
 }
+
+
+
+
 
 function render() {
     renderer.render(scene, camera.camera);
