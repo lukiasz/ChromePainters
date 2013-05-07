c4d4f4c30ae65b972a2b00535e91bad52e615584
klapek <jaszczakw@gmail.com>
2013-05-07 19:37:43 +0200

    addded - self- to brush place - fixed rotate

--- painterModel.js
+++ painterModel.js
@@ -6,7 +6,6 @@ function PainterModel(param) {
     this.colorHex = param.colorHex || "0x8888ff";
 	this.brushColor = param.brushColor;
     this.scene = param.scene;
-
 }
 
 PainterModel.prototype.init = function () {
@@ -14,31 +13,31 @@ PainterModel.prototype.init = function () {
     // zajmij sie tym w pierwszej kolejnosci, animacje zostaw
     // na pozniej. ThreeJS ma jakies loadery modelow, to powinno
     // byc stosunkowo proste
-	
 
-	 //'./models/' + this.color + 'Brush.dae'
-	 var x= this.xpos;
-	 var y = this.ypos;
-	 
-			var loader = new THREE.ColladaLoader();
-			loader.options.convertUpAxis = true;
-			loader.load( './models/' + this.brushColor + 'Brush.dae', function ( collada ) {
 
-				this.brush = collada.scene;
-				this.brush.doublesided = true;
-				this.brush.scale.x = this.brush.scale.y = this.brush.scale.z = 4;
-				this.brush.position.set(x,50,y);
-				this.brush.updateMatrix();
+    //'./models/' + this.color + 'Brush.dae'
+    var x = this.xpos;
+    var y = this.ypos;
 
-				this.scene.add(this.brush);
-				});
-		
-	
+    var loader = new THREE.ColladaLoader();
+    loader.options.convertUpAxis = true;
+    var self = this;
+    loader.load('./models/' + this.brushColor + 'Brush.dae', function (collada) {
+        this.brush = collada.scene;
+        this.brush.doublesided = true;
+        this.brush.scale.x = this.brush.scale.y = this.brush.scale.z = 4;
+        this.brush.position.set(x, 50, y);
+        this.brush.updateMatrix();
+        self.brush = this.brush;
+        this.scene.add(this.brush);
+    });
+
+
     /*var sphereGeometry = new THREE.CubeGeometry(50, 32, 16);
 
     // Justyna: zerknij tez przy okazji, czemu kolor mi nie dziala
     var sphereMaterial = new THREE.MeshLambertMaterial({
-        color: this.colorHex
+    color: this.colorHex
     });
     this.sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     this.sphere.position.set(this.xpos, 50, this.ypos);
